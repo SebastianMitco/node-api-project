720d50efd0a0ca9152dc06ec20105d42f1b758b6
SebastianMitco <70155015+SebastianMitco@users.noreply.github.com>
2022-07-23 09:23:20 +0200

    CRUD on Mongo DB functions in controllers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv  = require('dotenv')
 const morgan  = require('morgan')
+const colors  = require('colors')
 const connectDB = require('./config/db')
 
 //Load env vars
@@ -15,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
@@ -26,9 +30,9 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
     );
 
 //Handle unhandled promise rejections
