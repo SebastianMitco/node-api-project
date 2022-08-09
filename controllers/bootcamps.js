ad07502b2a9f6b4fca3665970aa6b7cc0693bcea
SebastianMitco <70155015+SebastianMitco@users.noreply.github.com>
2022-08-09 19:48:34 +0200

    Creating Error handler middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -26,7 +26,8 @@ exports.getBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ succes: false });
+    //res.status(400).json({ succes: false });
+    next(err);
   }
 };
 
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const dotenv  = require('dotenv')
-const morgan  = require('morgan')
-const colors  = require('colors')
-const connectDB = require('./config/db')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 //Load env vars
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
 //Connect DB
 connectDB();
 
 //Route files
-const bootcamps = require('./routes/bootcamps');
-
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -20,25 +20,27 @@ const app = express();
 app.use(express.json());
 
 //Dev logging middleware
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //Mount routers
 
-app.use('/api/v1/bootcamps', bootcamps);
-
+app.use("/api/v1/bootcamps", bootcamps);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
-    PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
-    );
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
 
 //Handle unhandled promise rejections
 
-process.on('unhandledRejection', (err,promise)=>{
-    console.log(`Error: ${err.message}`)
-    //Close server & exit process
-    server.close(()=> process.exit(1))
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
