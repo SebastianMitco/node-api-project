aa284f893b3678201974fcd2304b6f75e2088937
SebastianMitco <70155015+SebastianMitco@users.noreply.github.com>
2022-08-17 15:30:10 +0200

    Uloading photo on server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const fileupload = require("express-fileupload");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -24,6 +26,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//File uploading
+app.use(fileupload());
+
+//Set static folder
+//@desc     let you access files in domain.com/uploads/file_name.jpg
+//          becauese public is static folder - domain
+app.use(express.static(path.join(__dirname, "public")));
 
 //Mount routers
 
