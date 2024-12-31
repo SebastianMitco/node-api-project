0aaf8276bb3c68f4c98fd82a9230fc90ebeaf0ac
Sebastian Mitco <70155015+SebastianMitco@users.noreply.github.com>
2024-12-31 15:52:48 +0100

    advance recipe

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,4 +1,3 @@
-
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const Recipe = require("../models/Recipe");
@@ -8,44 +7,36 @@ const User = require("../models/User");
 // @route       GET /api/v1/recipes
 // @access      Private
 exports.getRecipes = asyncHandler(async (req, res, next) => {
-
-    req.body.user = req.user.id;
-    const user = await User.findById(req.user.id);
-    if (!user) {
-        return next(
-            new ErrorResponse(
-                `User is not authorized to create category`,
-                401
-            )
-        );
-    }
-    // Fetch recipes created by the authenticated user
-    const recipes = await Recipe.find({user: req.user.id});
-    res.status(201).json({
-        success: true,
-        data: recipes,
-    });
-
+  req.body.user = req.user.id;
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    return next(
+      new ErrorResponse(`User is not authorized to create category`, 401),
+    );
+  }
+  // Fetch recipes created by the authenticated user
+  const recipes = await Recipe.find({ user: req.user.id });
+  res.status(201).json({
+    success: true,
+    data: recipes,
+  });
 });
 
 // @desc        Create new Recipe
 // @route       POST /api/v1/recipes
 // @access      Private
 exports.createRecipe = asyncHandler(async (req, res, next) => {
-    req.body.user = req.user.id;
-    const user = await User.findById(req.user.id);
-    if (!user) {
-        return next(
-            new ErrorResponse(
-                `User is not authorized to create category`,
-                401
-            )
-        );
-    }
-    const category = await Recipe.create(req.body);
+  req.body.user = req.user.id;
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    return next(
+      new ErrorResponse(`User is not authorized to create category`, 401),
+    );
+  }
+  const category = await Recipe.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        data: category,
-    });
+  res.status(201).json({
+    success: true,
+    data: category,
+  });
 });
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,28 +1,47 @@
 const mongoose = require("mongoose");
 
 const RecipeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        trim: true,
-        required: [true, "Please add a recipe title"],
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add a recipe title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+  },
+  instructions: [
+    {
+      type: String,
+      required: true,
     },
-    description: {
-        type: String,
-        required: [true, "Please add a description"],
+  ],
+  cookingTime: {
+    type: Number,
+    required: true,
+    description: "The time in minutes required to cook the recipe.",
+  },
+  difficulty: {
+    type: String,
+    enum: ["easy", "medium", "hard"],
+    required: true,
+  },
+
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    categories: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-    }],
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true,
-    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Recipe", RecipeSchema);
